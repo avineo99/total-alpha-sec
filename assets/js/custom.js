ac48f74f58a3dacf1ab2f1dd18f0c9c253e4f1d9
avineo99 <avishek.neo99@gmail.com>
2025-04-01 12:45:07 +0530

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,75 @@
+
+$(document).ready(function () {
+	$(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('#ScrollToTop').fadeIn();
+        } else {
+            $('#ScrollToTop').fadeOut();
+        }
+		
+		if ($(window).width() > 991.98) {
+			 if ($(document).scrollTop() > 50) {
+				$('#Header').addClass('fixed-top');
+				$('#Header').addClass('shrink');
+			}
+			else {
+				$('#Header').removeClass('fixed-top');
+				$('#Header').removeClass('shrink');
+			}
+		}
+    });
+
+    $('#ScrollToTop').click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });  
+	
+	 
+	  
+});
+
+
+
+
+$('.testimonial-wrap').slick({
+	dots: false,
+	infinite: true,
+	arrows: false,
+	autoplay:true,
+	speed: 300,
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	responsive: [
+	  {
+		breakpoint: 991,
+		settings: {
+		  slidesToShow: 2,
+		  slidesToScroll: 1,
+		  infinite: true,
+		  dots: true
+		}
+	  },
+	  {
+		breakpoint: 600,
+		settings: {
+		  slidesToShow: 2,
+		  slidesToScroll: 2
+		}
+	  },
+	  {
+		breakpoint: 480,
+		settings: {
+		  slidesToShow: 1,
+		  slidesToScroll: 1
+		}
+	  }
+	  // You can unslick at a given breakpoint now by adding:
+	  // settings: "unslick"
+	  // instead of a settings object
+	]
+  });
+	  
+
+
+
+  
